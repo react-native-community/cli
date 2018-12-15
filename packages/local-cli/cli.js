b8b5c98ffff3411c7e55eeb7b0262ec3f8b9e04f
Mike Grabowski <grabbou@gmail.com>
2018-12-15 10:54:23 +0100

    Remove manual check of the node version

--- packages/local-cli/cli.js
+++ packages/local-cli/cli.js
@@ -12,10 +12,6 @@
 // gracefulify() has to be called before anything else runs
 require('graceful-fs').gracefulify(require('fs'));
 
-// This file must be able to run in node 0.12 without babel so we can show that
-// it is not supported. This is why the rest of the cli code is in `cliEntry.js`.
-require('./server/checkNodeVersion')();
-
 // Transpile the source code
 const babelConfig = require('./babel.config');
 require('@babel/register')(babelConfig);

802e4ec3f21c95d5f8875e0a9f106a4ab7fbd610
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2019-01-23 18:28:36 +0100

    feat: add logger (#113)
    Add simple logger.

---
+++ packages/cli/src/util/logger.js
@@ -0,0 +1,35 @@
+// @flow
+const chalk = require('chalk');
+
+const SEPARATOR = ', ';
+
+const joinMessages = (messages: Array<string>) => messages.join(SEPARATOR);
+
+const info = (...messages: Array<string>) => {
+  console.log(
+    `${chalk.black.bgCyan(' INFO ')} ${chalk.cyan(joinMessages(messages))}`
+  );
+};
+
+const warn = (...messages: Array<string>) => {
+  console.warn(
+    `${chalk.black.bgYellow(' WARN ')} ${chalk.yellow(joinMessages(messages))}`
+  );
+};
+
+const error = (...messages: Array<string>) => {
+  console.error(
+    `${chalk.black.bgRed(' ERROR ')} ${chalk.red(joinMessages(messages))}`
+  );
+};
+
+const debug = (...messages: Array<string>) => {
+  console.log(`${chalk.black.bgWhite(' DEBUG ')} ${joinMessages(messages)}`);
+};
+
+module.exports = {
+  info,
+  warn,
+  error,
+  debug,
+};

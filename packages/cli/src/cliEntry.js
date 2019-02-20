b37bca29f34c0613413d5fc0a9f9c99bd0eca1b4
Michał Pierzchała <thymikee@gmail.com>
2019-02-20 09:03:04 +0100

    fix: adjust platform check and wording

--- packages/cli/src/cliEntry.js
+++ packages/cli/src/cliEntry.js
@@ -139,15 +139,20 @@ async function run() {
 
 async function setupAndRun() {
   // We only have a setup script for UNIX envs currently
-  if (/^win/.test(process.platform) === false) {
+  if (process.platform !== 'win32') {
     const scriptName = 'setup_env.sh';
     const absolutePath = path.join(__dirname, '..', scriptName);
 
     try {
-      childProcess.execFileSync(absolutePath);
+      childProcess.execFileSync(absolutePath, { stdio: 'pipe' });
     } catch (error) {
       logger.warn(
-        `Failed to run environment setup script "${scriptName}". This script can continue to run if your local environment matches what React Native expects. If it does fail, check out "${absolutePath}" and adjust your environment to match it.\n${error}`
+        `Failed to run environment setup script "${scriptName}".\n\n${chalk.red(
+          error
+        )}`
+      );
+      logger.info(
+        `React Native CLI will continue to run if your local environment matches what React Native expects. If it does fail, check out "${absolutePath}" and adjust your environment to match it.`
       );
     }
   }

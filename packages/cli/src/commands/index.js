8e0ed9e6097504edd331f422ae5ca78bb5880ff8
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2019-03-28 21:29:09 +0100

    Refactor: Remove eject (#275)
    Summary:
    ---------
    
    Since `create-react-native-app` is deprecated this functionality is no longer needed.

--- packages/cli/src/commands/index.js
+++ packages/cli/src/commands/index.js
@@ -19,7 +19,6 @@ import runAndroid from './runAndroid/runAndroid';
 import library from './library/library';
 import bundle from './bundle/bundle';
 import ramBundle from './bundle/ramBundle';
-import eject from './eject/eject';
 import link from './link/link';
 import unlink from './link/unlink';
 import install from './install/install';
@@ -40,7 +39,6 @@ const loadLocalCommands: Array<LocalCommandT> = [
   library,
   bundle,
   ramBundle,
-  eject,
   link,
   unlink,
   install,

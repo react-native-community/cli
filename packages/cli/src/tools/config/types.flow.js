3b49d80ca89c7b61af79b25d9aa1d2750c5f36df
Mike Grabowski <grabbou@gmail.com>
2019-04-02 21:59:43 +0200

    Describe the types

--- packages/cli/src/tools/config/types.flow.js
+++ packages/cli/src/tools/config/types.flow.js
@@ -2,8 +2,18 @@
  * @flow
  */
 
+/**
+ * Opaque type that describes the Inquirer question format. Not typed, since we just
+ * pass it directly to Inquirer. Validation is done with Joi in `schema.js`
+ */
 type InquirerPromptT = any;
 
+/**
+ * Settings that a library author can define in the configuration bundled with
+ * dependency for Android
+ *
+ * See UserDependencyConfigT for details
+ */
 type DependencyParamsAndroidT = {
   sourceDir?: string,
   manifestPath?: string,
@@ -11,6 +21,11 @@ type DependencyParamsAndroidT = {
   packageInstance?: string,
 };
 
+/**
+ * Settings that user can define in the project configuration for Android
+ *
+ * See UserConfigT for details
+ */
 type ProjectParamsAndroidT = {
   sourceDir?: string,
   manifestPath?: string,
@@ -23,21 +38,39 @@ type ProjectParamsAndroidT = {
   buildGradlePath?: string,
 };
 
+/**
+ * Settings that user can define in the project configuration for iOS.
+ * Same for dependency - we share the type.
+ *
+ * See UserDependencyConfigT and UserConfigT for details
+ */
 type ProjectParamsIOST = {
   project?: string,
   sharedLibraries?: string[],
   libraryFolder?: string,
 };
 
+/**
+ * Final configuration object
+ */
 export type ConfigT = {|
+  // Root where the configuration has been resolved from
   root: string,
+
+  // Path to React Native source
   reactNativePath: string,
+
+  // Object that contains configuration for a project (null, when platform not available)
   project: {
     android: ?ProjectConfigAndroidT,
     ios: ?ProjectConfigIOST,
     [key: string]: any,
   },
+
+  // An array of assets as defined by the user
   assets: string[],
+
+  // Map of the dependencies that are present in the project
   dependencies: {
     [key: string]: {
       name: string,
@@ -55,6 +88,8 @@ export type ConfigT = {|
       params: InquirerPromptT[],
     },
   },
+
+  // Map of available platforms (built-ins and dynamically loaded)
   platforms: {
     [name: string]: any,
     ios: {
@@ -114,25 +149,33 @@ export type ConfigT = {|
       },
     },
   },
+
+  // An array of commands that are present in 3rd party packages
   commands: string[],
+
+  // Haste configuration resolved based on available plugins
   haste: {
     platforms: Array<string>,
     providesModuleNodeModules: Array<string>,
   },
 |};
 
+/**
+ * Aliases
+ */
 export type DependencyConfigT = $PropertyType<
   $PropertyType<ConfigT, 'dependencies'>,
   '[key: string]',
 >;
-
 export type HooksT = $PropertyType<DependencyConfigT, 'hooks'>;
-
 export type ProjectConfigT = $PropertyType<ConfigT, 'project'>;
-
 export type PlatformsT = $PropertyType<ConfigT, 'platforms'>;
 
+/**
+ * Config defined by a developer for a library
+ */
 export type UserDependencyConfigT = {
+  // Additional dependency settings
   dependency: {
     platforms: {
       android: DependencyParamsAndroidT,
@@ -143,15 +186,28 @@ export type UserDependencyConfigT = {
     hooks: HooksT,
     params: InquirerPromptT[],
   },
+
+  // An array of commands that ship with the dependency
   commands: string[],
+
+  // An array of extra platforms to load
   platforms: {
     [name: string]: any,
   },
 };
 
+/**
+ * Config defined by a developer for the project
+ */
 export type UserConfigT = {
+  /**
+   * Shares some structure with ConfigT, except that haste, root, platforms
+   * are calculated and can't be defined
+   */
   ...$Diff<ConfigT, {haste: any, root: any, platforms: any}>,
   reactNativePath: ?string,
+
+  // Additional project settings
   project: {
     android?: ProjectParamsAndroidT,
     ios?: ProjectParamsIOST,

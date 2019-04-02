416c0bb371ead94208869da8489867ba552b4540
Mike Grabowski <grabbou@gmail.com>
2019-04-02 20:27:03 +0200

    Fix formatting and update snapshot

--- packages/cli/src/commands/info/__tests__/info.test.js
+++ packages/cli/src/commands/info/__tests__/info.test.js
@@ -17,16 +17,20 @@ beforeEach(() => {
 
 const config = loadConfig();
 
-test('prints output without arguments', async () => {
-  await info.func([], config, {});
-  expect(logger.info).toHaveBeenCalledWith(
-    'Fetching system and libraries information...',
-  );
-  const output = (logger.log: any).mock.calls[0][0];
-  // Checking on output that should be present on all OSes.
-  // TODO: move to e2e tests and adjust expectations to include npm packages
-  expect(output).toContain('System:');
-  expect(output).toContain('Binaries:');
-}, 20000);
+test(
+  'prints output without arguments',
+  async () => {
+    await info.func([], config, {});
+    expect(logger.info).toHaveBeenCalledWith(
+      'Fetching system and libraries information...',
+    );
+    const output = (logger.log: any).mock.calls[0][0];
+    // Checking on output that should be present on all OSes.
+    // TODO: move to e2e tests and adjust expectations to include npm packages
+    expect(output).toContain('System:');
+    expect(output).toContain('Binaries:');
+  },
+  20000,
+);
 
 test.todo('prints output with --packages');

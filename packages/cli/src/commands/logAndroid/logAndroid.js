834f702354ff6f5d350fd5b3266d56f79e3cee98
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2019-04-05 22:55:39 +0200

    Feature: Use logkitty to format android logs

--- packages/cli/src/commands/logAndroid/logAndroid.js
+++ packages/cli/src/commands/logAndroid/logAndroid.js
@@ -5,26 +5,19 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import {spawnSync} from 'child_process';
+import execa from 'execa';
 import logger from '../../tools/logger';
 
-/**
- * Starts adb logcat
- */
 async function logAndroid() {
-  const adbPath = process.env.ANDROID_HOME
-    ? `${process.env.ANDROID_HOME}/platform-tools/adb`
-    : 'adb';
+  logger.info('Starting logkitty');
 
-  const adbArgs = ['logcat', '*:S', 'ReactNative:V', 'ReactNativeJS:V'];
-
-  logger.info(`Starting the logger (${adbPath} ${adbArgs.join(' ')})...`);
-
-  const log = spawnSync(adbPath, adbArgs, {stdio: 'inherit'});
-
-  if (log.error !== null) {
-    throw log.error;
-  }
+  await execa(
+    'logkitty',
+    ['custom', '*:S', 'ReactNative:V', 'ReactNativeJS:V'],
+    {
+      stdio: 'inherit',
+    },
+  );
 }
 
 export default {

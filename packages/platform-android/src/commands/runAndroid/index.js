8ea298931f20479f1ad6d325b2ba3914a9c638a5
ifsnow <ifsnow@gmail.com>
2019-04-19 10:05:34 +0900

    fix: modify wrong install arguments

--- packages/platform-android/src/commands/runAndroid/index.js
+++ packages/platform-android/src/commands/runAndroid/index.js
@@ -173,9 +173,9 @@ function tryInstallAppOnDevice(args, adbPath, device) {
     );
 
     const pathToApk = `${buildDirectory}/${apkFile}`;
-    const adbArgs = ['-s', device, 'install', '-rd', pathToApk];
+    const adbArgs = ['-s', device, 'install', '-r', '-d', pathToApk];
     logger.info(
-      `Installing the app on the device (cd android && adb -s ${device} install -rd ${pathToApk}`,
+      `Installing the app on the device (cd android && adb -s ${device} install -r -d ${pathToApk}`,
     );
     execFileSync(adbPath, adbArgs, {
       stdio: [process.stdin, process.stdout, process.stderr],

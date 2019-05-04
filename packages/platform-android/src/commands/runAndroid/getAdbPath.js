be275d294be6bc715757d3a4f1258eb83d8df1d9
Minhyuk Kim <kmh011225@naver.com>
2019-05-04 17:01:26 +0900

    fix:don\`t run app on devices when finished run-android build

--- packages/platform-android/src/commands/runAndroid/getAdbPath.js
+++ packages/platform-android/src/commands/runAndroid/getAdbPath.js
@@ -1,16 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true,
+});
+exports.default = void 0;
+
 /**
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow
+ *
  */
-
 function getAdbPath() {
   return process.env.ANDROID_HOME
     ? `${process.env.ANDROID_HOME}/platform-tools/adb`
-    : 'adb';
+    : `C:/Users/${
+        require("os").userInfo().username
+      }/AppData/Local/Android/Sdk/platform-tools/adb`;
 }
 
-export default getAdbPath;
+var _default = getAdbPath;
+exports.default = _default;

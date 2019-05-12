6ebac9f72ac133e5679f08dc9f463412742478b1
Michał Pierzchała <thymikee@gmail.com>
2019-05-12 10:09:43 +0200

    fix: run "config" from react-native binary

--- packages/platform-ios/native_modules.rb
+++ packages/platform-ios/native_modules.rb
@@ -5,12 +5,10 @@
 #
 def use_native_modules!(root = "..", packages = nil)
   if (!packages)
-    # Resolve the CLI's main index file
-    cli_bin = Pod::Executable.execute_command("node", ["-e", "console.log(require.resolve('@react-native-community/cli/build/index.js'))"], true).strip
     output = ""
     # Make sure `react-native config` is ran from your project root
     Dir.chdir(root) do
-      output = Pod::Executable.execute_command("node", [cli_bin, "config"], true)
+      output = Pod::Executable.execute_command("node", ["./node_modules/.bin/react-native", "config"], true)
     end
 
     json = []

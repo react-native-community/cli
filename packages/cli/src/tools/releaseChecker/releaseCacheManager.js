043640f1f875aada2103ff9e6a69aac3732dac29
Matei Radu <matei.radu.92@gmail.com>
2019-05-25 10:13:19 +0200

    Create cache dir in os.homedir()

--- packages/cli/src/tools/releaseChecker/releaseCacheManager.js
+++ packages/cli/src/tools/releaseChecker/releaseCacheManager.js
@@ -3,13 +3,19 @@
  */
 import path from 'path';
 import fs from 'fs';
+import os from 'os';
 import logger from '../logger';
+import loadConfig from '../config';
 
 type ReleaseCacheKey = 'eTag' | 'lastChecked' | 'latestVersion';
 
 function loadCache(): ?Object {
   try {
-    const cacheRaw = fs.readFileSync(path.resolve(__dirname, '.cache'), 'utf8');
+    const {name} = require(path.join(loadConfig().root, 'package.json'));
+    const cacheRaw = fs.readFileSync(
+      path.resolve(getCacheRootPath(), name),
+      'utf8',
+    );
     const cache = JSON.parse(cacheRaw);
     return cache;
   } catch (e) {
@@ -22,10 +28,25 @@ function loadCache(): ?Object {
 }
 
 function saveCache(cache: Object) {
+  const {name} = require(path.join(loadConfig().root, 'package.json'));
   fs.writeFileSync(
-    path.resolve(__dirname, '.cache'),
+    path.resolve(getCacheRootPath(), name),
     JSON.stringify(cache, null, 2),
   );
+}
+
+/**
+ * Returns the path string of `$HOME/.react-native-cli`.
+ *
+ * In case it doesn't exist, it will be created.
+ */
+function getCacheRootPath() {
+  const cachePath = path.resolve(os.homedir(), '.react-native-cli', 'cache');
+  if (!fs.existsSync(cachePath)) {
+    fs.mkdirSync(cachePath);
+  }
+
+  return cachePath;
 }
 
 function get(key: ReleaseCacheKey): any {

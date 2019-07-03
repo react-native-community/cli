778041dd9fb29ec759a16a2281383efb44e949c0
Rohan Talip <RohanTalip@users.noreply.github.com>
2019-07-03 16:48:45 -0700

    Update packages/cli/src/tools/config/index.js
    Co-Authored-By: Kacper Wiszczuk <kacperwiszczuk@gmail.com>

--- packages/cli/src/tools/config/index.js
+++ packages/cli/src/tools/config/index.js
@@ -196,7 +196,7 @@ function loadConfig(projectRoot: string = process.cwd()): ConfigT {
         .join(
           '\n',
         )}\nPlease notify their maintainers about it. You can find more details at ${chalk.dim.underline(
-        'https://github.com/react-native-community/cli/blob/master/docs/configuration.md',
+        'https://github.com/react-native-community/cli/blob/master/docs/configuration.md#migration-guide',
       )}.`,
     );
   }

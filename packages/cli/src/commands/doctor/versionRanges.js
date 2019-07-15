74f818151e7c58f04cf5cdd83777b5dfcfb268f2
Lucas Bento <lucas.bsilva@outlook.com>
2019-07-15 10:14:40 +0200

    Add basic version ranges

---
+++ packages/cli/src/commands/doctor/versionRanges.js
@@ -0,0 +1,11 @@
+export default {
+  // Common
+  NODE_JS: '>= 11 <= 12', //'10.x',
+  YARN: '>= 1.10.x',
+  NPM: '>= 4.x',
+  WATCHMAN: '4.x',
+  // Android
+  ANDROID_NDK: '',
+  // iOS
+  XCODE: '10.x',
+};

901597d0590de2dac2fdad594277371c24b9ef74
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2019-07-16 22:25:31 +0200

    Impl suggestions

--- packages/cli/src/commands/init/errors/HelloWorldError.js
+++ packages/cli/src/commands/init/errors/HelloWorldError.js
@@ -2,7 +2,7 @@
 export default class HelloWorldError extends Error {
   constructor() {
     super(
-      'Project name shouldn\'t contain "HelloWorld" name in it, because it is CLI\'s default placeholder name. You can find more information https://github.com/react-native-community/cli/issues/441',
+      'Project name shouldn\'t contain "HelloWorld" name in it, because it is CLI\'s default placeholder name.',
     );
   }
 }

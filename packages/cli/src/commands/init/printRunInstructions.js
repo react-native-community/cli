062f02bddb145c7cfba241d6e77a166aa9a463b3
Taym Haddadi <bentaimia.haddadi@signify.com>
2019-07-16 10:32:29 +0200

    fix: shorter and correct paths in init run instructions (#533)

--- packages/cli/src/commands/init/printRunInstructions.js
+++ packages/cli/src/commands/init/printRunInstructions.js
@@ -14,8 +14,8 @@ import chalk from 'chalk';
 import {logger} from '@react-native-community/cli-tools';
 
 function printRunInstructions(projectDir: string, projectName: string) {
-  const absoluteProjectDir = path.resolve(projectDir);
-  const iosProjectDir = path.resolve(projectDir, 'ios');
+  const relativeProjectDir = path.relative(projectDir, projectName);
+  const iosProjectDir = path.resolve(projectDir, `${projectName}/ios`);
 
   const iosPodsFile = path.resolve(iosProjectDir, `${projectName}.xcworkspace`);
   const isUsingPods = fs.existsSync(iosPodsFile);
@@ -29,14 +29,14 @@ function printRunInstructions(projectDir: string, projectName: string) {
 
   logger.log(`
   ${chalk.cyan(`Run instructions for ${chalk.bold('iOS')}`)}:
-    • cd ${absoluteProjectDir} && react-native run-ios
+    • cd ${relativeProjectDir} && react-native run-ios
     - or -
     • Open ${relativeXcodeProjectPath} in Xcode or run "xed -b ios"
     • Hit the Run button
 
   ${chalk.green(`Run instructions for ${chalk.bold('Android')}`)}:
     • Have an Android emulator running (quickest way to get started), or a device connected.
-    • cd ${absoluteProjectDir} && react-native run-android
+    • cd ${relativeProjectDir} && react-native run-android
 `);
 }
 

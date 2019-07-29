762ab518771686ac1605706f7a46516ff6ee4fc5
Lucas Bento <lucas.bsilva@outlook.com>
2019-07-29 11:19:11 +0200

    Add automatic fix for `CocoaPods`

--- packages/cli/src/commands/doctor/issues/cocoaPods.js
+++ packages/cli/src/commands/doctor/issues/cocoaPods.js
@@ -1,3 +1,4 @@
+import execa from 'execa';
 import {isSoftwareInstalled} from '../checkInstallation';
 
 const delay = amount => new Promise(resolve => setTimeout(resolve, amount));
@@ -7,7 +8,22 @@ const cocoaPods = {
   getDiagnosticsAsync: async () => ({
     needsToBeFixed: !(await isSoftwareInstalled('pod')),
   }),
-  runAutomaticFix: () => delay(5000),
+  runAutomaticFix: async ({loader}) => {
+    try {
+      // First attempt to install `cocoapods`
+      await execa('gem', ['install', 'cocoapods']);
+    } catch (_error) {
+      // TODO: find a way to not need this as it breaks the flow of the UI
+      // maybe by looking into the `process.stdout`
+      // `loader` needs to stop in order to show the "password request" if running with sudo
+      loader.stop();
+
+      throw new Error();
+
+      // If that doesn't work then try with sudo
+      // await execa('sudo', ['gem', 'install', 'cocoapods']);
+    }
+  },
 };
 
 export default cocoaPods;

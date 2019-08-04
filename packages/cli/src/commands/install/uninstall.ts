c4f1f467108ebc4a31ce77514605c1b35b6a54f3
thecodrr <enkaboot@gmail.com>
2019-08-04 05:43:19 +0500

    refactor: check package before uninstalling

--- packages/cli/src/commands/install/uninstall.ts
+++ packages/cli/src/commands/install/uninstall.ts
@@ -9,9 +9,28 @@ import {Config} from '@react-native-community/cli-types';
 import {logger} from '@react-native-community/cli-tools';
 import * as PackageManager from '../../tools/packageManager';
 import unlink from '../link/unlink';
+import chalk from 'chalk';
 
 async function uninstall(args: Array<string>, ctx: Config): Promise<void> {
   const name = args[0];
+
+  if (!Object.keys(ctx.dependencies).includes(name)) {
+    logger.error(
+      `Unknown package name "${chalk.bgRed.dim(
+        name,
+      )}". The package you are trying to uninstall is not present in your "package.json" dependencies.`,
+    );
+    logger.info(
+      `${chalk.bold(
+        'We found the following dependencies installed in your project:\n',
+      )}${chalk.green.dim(
+        Object.keys(ctx.dependencies)
+          .map(dependency => `     - ${dependency}`)
+          .join('\n'),
+      )}`,
+    );
+    process.exit();
+  }
 
   logger.info(`Unlinking "${name}"...`);
   await unlink.func([name], ctx, {});

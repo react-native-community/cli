70810e11b0b54ba0b73682f6c1c9d14406d0bae0
Lucas Bento <lucas.bsilva@outlook.com>
2019-08-05 13:15:32 +0200

    Change manual installation messages

--- packages/cli/src/commands/doctor/issues/common.js
+++ packages/cli/src/commands/doctor/issues/common.js
@@ -4,12 +4,16 @@ import chalk from 'chalk';
 const logManualInstallation = ({issue, url, command}) => {
   if (url) {
     // Space is necessary to keep correct ordering on screen
-    return logger.log(`   Download ${issue} manually at ${chalk.dim(url)}.`);
+    return logger.log(
+      `   Read more about how to download ${issue} at ${chalk.dim(url)}.`,
+    );
   }
 
   if (command) {
     // Space is necessary to keep correct ordering on screen
-    return logger.log(`   Install ${issue} by running ${chalk.dim(command)}.`);
+    return logger.log(
+      `   Please install ${issue} by running ${chalk.dim(command)}.`,
+    );
   }
 };
 

5d23c71f186e6d8739cfa57b378526f8146e9c0b
Christoph Nakazawa <cpojer@fb.com>
2019-08-15 14:02:03 +0100

    Remove "."

--- packages/cli/src/commands/init/banner.js
+++ packages/cli/src/commands/init/banner.js
@@ -34,7 +34,7 @@ const reactLogoArray = [
 const welcomeMessage =
   '                  Welcome to React Native!                ';
 const learnOnceMessage =
-  '                 Learn once, write anywhere.              ';
+  '                 Learn once, write anywhere               ';
 
 export default `${chalk.blue(reactLogoArray.join('\n'))}
 

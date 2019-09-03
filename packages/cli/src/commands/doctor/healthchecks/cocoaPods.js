7b9b08ad5c40b1f2bb0b10b7870ea73a0af5a33d
Tommy Nguyen <tonguye@microsoft.com>
2019-09-03 16:56:13 +0200

    doctor: Reuse installCocoaPods() and add Homebrew option

--- packages/cli/src/commands/doctor/healthchecks/cocoaPods.js
+++ packages/cli/src/commands/doctor/healthchecks/cocoaPods.js
@@ -1,52 +1,10 @@
-import execa from 'execa';
 import {isSoftwareInstalled} from '../checkInstallation';
-
-const hasSudoGranted = async () => {
-  try {
-    await execa('sudo', ['-nv']);
-
-    return true;
-  } catch (_ignored) {
-    return false;
-  }
-};
-
-// This is to show the loader but clear the `Password` prompt from the screen
-const reportLoader = async ({loader, type, userHasSudoGranted}) => {
-  process.stdout.moveCursor(0, userHasSudoGranted ? -1 : -2);
-  process.stdout.clearScreenDown();
-
-  loader[type]();
-};
+import {installCocoaPods} from '../../../tools/installPods';
 
 export default {
   label: 'CocoaPods',
   getDiagnosticsAsync: async () => ({
     needsToBeFixed: !(await isSoftwareInstalled('pod')),
   }),
-  runAutomaticFix: async ({loader}) => {
-    try {
-      // First attempt to install `cocoapods`
-      await execa('gem', ['install', 'cocoapods']);
-
-      loader.succeed();
-    } catch (_error) {
-      // This stops the loader so the user can provide a password to the `sudo` command
-      loader.stopAndPersist();
-
-      const userHasSudoGranted = await hasSudoGranted();
-
-      try {
-        await execa('sudo', ['gem', 'install', 'cocoapods']);
-
-        return await reportLoader({
-          loader,
-          type: 'succeed',
-          userHasSudoGranted,
-        });
-      } catch (_ignored) {
-        return await reportLoader({loader, type: 'fail', userHasSudoGranted});
-      }
-    }
-  },
+  runAutomaticFix: async ({loader}) => await installCocoaPods(loader),
 };

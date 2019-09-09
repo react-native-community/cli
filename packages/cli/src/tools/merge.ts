6e61a0b59e349c5127cd971bc95a7ce3c29e81b8
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-09-10 08:41:43 +0900

    chore: add type to deepmerge(#683)

--- packages/cli/src/tools/merge.ts
+++ packages/cli/src/tools/merge.ts
@@ -1,15 +1,15 @@
-/**
- * @flow
- */
-
 import deepmerge from 'deepmerge';
 
 /**
  * `deepmerge` concatenates arrays by default instead of overwriting them.
  * We define custom merging function for arrays to change that behaviour
  */
-export default function merge(...objs: Array<{[key: string]: any}>) {
-  return deepmerge(...objs, {
-    arrayMerge: (destinationArray, sourceArray, options) => sourceArray,
+export default function merge(
+  x: Array<{[key: string]: any}>,
+  y: Array<{[key: string]: any}>,
+) {
+  return deepmerge(x, y, {
+    arrayMerge: (destinationArray: any[], sourceArray: any[]): any[] =>
+      sourceArray,
   });
 }

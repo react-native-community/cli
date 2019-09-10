df18b51031dde032118a45c1911a377db58a4024
quadriphobs1 <abiodunquadriadekunle@gmail.com>
2019-09-10 16:13:35 +0100

    disable eslint for concerned line

--- packages/cli/src/commands/bundle/buildBundle.ts
+++ packages/cli/src/commands/bundle/buildBundle.ts
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
-/* eslint-disable import/namespace, import/default */
 
 // @ts-ignore - no typed definition for the package
 import Server from 'metro/src/Server';

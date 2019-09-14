dfb94f1c0db7bf7b28356977bd04771730929052
anikethsaha <anik220798@gmail.com>
2019-09-14 19:58:24 +0530

    chore: removed the inquirer type and moved the node type to 8

--- packages/cli/src/commands/link/pollParams.ts
+++ packages/cli/src/commands/link/pollParams.ts
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+// @ts-ignore untyped
 import {prompt, QuestionCollection, Answers} from 'inquirer';
 
 export default (questions: QuestionCollection) =>

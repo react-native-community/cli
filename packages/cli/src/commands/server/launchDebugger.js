9b271b3106b2c48d73209df2aea6146cf7e923f3
Bentaimia Haddadi <bentaimia.haddadi@signify.com>
2019-10-04 16:45:21 +0200

    refactor: make it simple

--- packages/cli/src/commands/server/launchDebugger.js
+++ packages/cli/src/commands/server/launchDebugger.js
@@ -26,28 +26,6 @@ function commandExistsUnixSync(commandName) {
   }
 }
 
-function commandExistsWindowsSync(commandName) {
-  try {
-    const stdout = execSync('where ' + commandName, {stdio: []});
-    return !!stdout;
-  } catch (error) {
-    return false;
-  }
-}
-
-function commandExists(commandName) {
-  switch (process.platform) {
-    case 'win32':
-      return commandExistsWindowsSync(commandName);
-    case 'linux':
-    case 'darwin':
-      return commandExistsUnixSync(commandName);
-    default:
-      // assume it doesn't exist, just to be safe.
-      return false;
-  }
-}
-
 function getChromeAppName(): string {
   switch (process.platform) {
     case 'darwin':
@@ -68,45 +46,23 @@ function getChromeAppName(): string {
   }
 }
 
-function getChromeCommandName(): string {
-  switch (process.platform) {
-    case 'darwin':
-      return '/Applications/Google" "Chrome.app/Contents/MacOS/Google" "Chrome';
-    case 'win32':
-      return 'chrome';
-    case 'linux':
-      if (commandExistsUnixSync('google-chrome')) {
-        return 'google-chrome';
-      }
-      if (commandExistsUnixSync('chromium-browser')) {
-        return 'chromium-browser';
-      }
-      return 'chromium';
-
-    default:
-      return 'google-chrome';
+async function launchChrome(url: string) {
+  const {err} = await open(url, {app: getChromeAppName()});
+  if (err) {
+    logger.error('Google Chrome exited with error:', err);
   }
 }
 
-function launchChrome(url: string) {
-  open(url, {app: [getChromeAppName()]}, err => {
-    if (err) {
-      logger.error('Google Chrome exited with error:', err);
-    }
-  });
-}
-
-function launchDebugger(url: string) {
-  if (!commandExists(getChromeCommandName())) {
-    logger.info(
-      `For a better debugging experience please install Google Chrome from: ${chalk.underline.dim(
-        'https://www.google.com/chrome/',
-      )}`,
-    );
+async function launchDebugger(url: string) {
+  try {
+    await launchChrome(url);
+  } catch (error) {
     launchDefaultBrowser(url);
-    return;
+    logger.info(
+      `For a better debugging experience please install Google Chrome from: 	 
+        ${chalk.underline.dim('https://www.google.com/chrome/')}`,
+    );
   }
-  launchChrome(url);
 }
 
 export default launchDebugger;

553df50fd4bfea144cfbfa568fd9af644d9002cd
grabbou <grabbou@gmail.com>
2019-10-04 15:17:15 +0200

    Add tests - one is failing, need to check why

---
+++ packages/cli/src/tools/config/__tests__/findProjectRoot-test.ts
@@ -0,0 +1,43 @@
+import path from 'path';
+import findProjectRoot from '../findProjectRoot';
+import {
+  cleanup,
+  writeFiles,
+  getTempDirectory,
+} from '../../../../../../jest/helpers';
+
+beforeEach(() => {
+  cleanup(DIR);
+  jest.resetModules();
+});
+
+afterEach(() => cleanup(DIR));
+
+const DIR = getTempDirectory('find_project_root_test');
+
+test('resolves to correct project root', () => {
+  writeFiles(DIR, {
+    'package.json': '{}',
+    ios: {
+      Podfile: '',
+    },
+  });
+  const cwd = path.join(DIR, 'ios');
+  expect(findProjectRoot(cwd)).toBe(DIR);
+});
+
+test('resolves to correct project root in a monorepo', () => {
+  writeFiles(DIR, {
+    'package.json': '{}',
+    packages: {
+      mobile: {
+        'package.json': '{}',
+        ios: {
+          Podfile: '',
+        },
+      },
+    },
+  });
+  const cwd = path.join(DIR, 'packages/mobile/ios');
+  expect(findProjectRoot(cwd)).toBe(path.join(DIR, 'packages/mobile'));
+});

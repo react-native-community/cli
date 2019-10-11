62ee6a36459b07a9d354b52c0b54c024ed45baf6
Moti Zilberman <motiz88@gmail.com>
2019-10-11 10:56:15 +0100

    Fix typo in regex

--- packages/cli/src/tools/loadMetroConfig.ts
+++ packages/cli/src/tools/loadMetroConfig.ts
@@ -29,7 +29,7 @@ const INTERNAL_CALLSITES_REGEX = new RegExp(
   [
     '/Libraries/Renderer/implementations/.+\\.js$',
     '/Libraries/BatchedBridge/MessageQueue\\.js$',
-    '/Libraries/YellowBox/.+\.js$',
+    '/Libraries/YellowBox/.+\\.js$',
     '/node_modules/react-devtools-core/.+\\.js$',
   ].join('|'),
 );

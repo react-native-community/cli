2fec727ae4cacd33042049718e678999563bad49
grabbou <grabbou@gmail.com>
2019-10-17 12:15:23 +0200

    Add deprecated commands

--- packages/cli-types/src/ios.ts
+++ packages/cli-types/src/ios.ts
@@ -25,7 +25,7 @@ export interface IOSProjectConfig {
   sourceDir: string;
   scriptPhases: Array<ScriptPhase>;
 
-  podfile?: string;
+  podfile: string | undefined;
 }
 
 export interface IOSDependencyConfig {
@@ -35,5 +35,5 @@ export interface IOSDependencyConfig {
     path: string;
     execution_position: string;
   }>;
-  podspecPath?: string;
+  podspecPath: string | undefined;
 }
--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -10,6 +10,24 @@ import config from './config/config';
 import init from './init';
 // @ts-ignore - JS file
 import doctor from './doctor';
+import {CLIError} from '@react-native-community/cli-tools';
+
+const makeDeprecatedCommand = (name: string) => {
+  return {
+    name,
+    func: () => {
+      throw new CLIError(`
+        This command has been removed in favor of "autolinking" with this React Native release.
+        
+        Check https://github.com/react-native-community/cli/blob/master/docs/autolinking.md
+        for more details.
+
+        If you still need to use "react-native ${name}" command, consider installing
+        React Native CLI 3.x.
+      `);
+    },
+  };
+};
 
 export const projectCommands = [
   server,
@@ -19,6 +37,12 @@ export const projectCommands = [
   info,
   config,
   doctor,
+
+  // Deprecated commands
+  makeDeprecatedCommand('link'),
+  makeDeprecatedCommand('unlink'),
+  makeDeprecatedCommand('install'),
+  makeDeprecatedCommand('uninstall'),
 ] as Command[];
 
 export const detachedCommands = [init] as DetachedCommand[];

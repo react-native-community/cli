ea4ffd827cd6344d4f9c867ce64f5159526b2669
Michał Pierzchała <thymikee@gmail.com>
2019-11-28 17:49:14 +0100

    mock existsSync once

--- packages/cli/src/commands/init/__tests__/templateName.test.ts
+++ packages/cli/src/commands/init/__tests__/templateName.test.ts
@@ -5,19 +5,19 @@ const RN_NPM_PACKAGE = 'react-native';
 const ABS_RN_PATH = '/path/to/react-native';
 const ABS_RN_PATH_WINDOWS = 'path/to/react-native';
 
-test('supports file protocol with absolute path', async () => {
+test('supports file protocol with absolute path', () => {
   jest.spyOn(fs, 'existsSync').mockImplementationOnce(() => true);
   jest
     .spyOn(fs, 'readFileSync')
     .mockImplementationOnce(() => JSON.stringify({name: 'react-native'}));
-  expect(await processTemplateName(`file://${ABS_RN_PATH}`)).toEqual({
+  expect(processTemplateName(`file://${ABS_RN_PATH}`)).toEqual({
     uri: process.platform === 'win32' ? ABS_RN_PATH_WINDOWS : ABS_RN_PATH,
     name: RN_NPM_PACKAGE,
   });
 });
 
-test('supports npm packages as template names', async () => {
-  expect(await processTemplateName(RN_NPM_PACKAGE)).toEqual({
+test('supports npm packages as template names', () => {
+  expect(processTemplateName(RN_NPM_PACKAGE)).toEqual({
     uri: RN_NPM_PACKAGE,
     name: RN_NPM_PACKAGE,
   });
@@ -32,16 +32,16 @@ test.each`
   ${'@scoped/name@tag'}    | ${'@scoped/name@tag'}    | ${'@scoped/name'}
 `(
   'supports versioned npm package "$templateName" as template name',
-  async ({templateName, uri, name}) => {
-    expect(await processTemplateName(templateName)).toEqual({uri, name});
+  ({templateName, uri, name}) => {
+    expect(processTemplateName(templateName)).toEqual({uri, name});
   },
 );
 
-test('supports path to tgz archives', async () => {
+test('supports path to tgz archives', () => {
   const ABS_RN_TARBALL_PATH =
     '/path/to/react-native/react-native-1.2.3-rc.0.tgz';
-  jest.spyOn(fs, 'existsSync').mockImplementation(() => true);
-  expect(await processTemplateName(`file://${ABS_RN_TARBALL_PATH}`)).toEqual({
+  jest.spyOn(fs, 'existsSync').mockImplementationOnce(() => true);
+  expect(processTemplateName(`file://${ABS_RN_TARBALL_PATH}`)).toEqual({
     uri: `file://${ABS_RN_TARBALL_PATH}`,
     name: 'react-native',
   });

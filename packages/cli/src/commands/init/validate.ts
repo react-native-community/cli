a23dca0412b9418cf506c1747d240dd8c1eaad78
Sunny Luo <sunnylqm@qq.com>
2019-11-30 23:08:05 +0800

    Check java keywords when init new project

--- packages/cli/src/commands/init/validate.ts
+++ packages/cli/src/commands/init/validate.ts
@@ -3,13 +3,17 @@ import ReservedNameError from './errors/ReservedNameError';
 import HelloWorldError from './errors/HelloWorldError';
 
 const NAME_REGEX = /^[$A-Z_][0-9A-Z_$]*$/i;
+// ref: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html
+const javaKeywords = ["abstract", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "synchronized", "boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw", "byte", "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally", "long", "strictfp", "volatile", "const", "float", "native", "super", "while"];
+const reservedNames = ['react', 'react-native', ...javaKeywords];
 
 export function validateProjectName(name: string) {
   if (!String(name).match(NAME_REGEX)) {
     throw new InvalidNameError(name);
   }
 
-  if (name === 'React' || name === 'react') {
+  const lowerCaseName = name.toLowerCase();
+  if (reservedNames.includes(lowerCaseName)) {
     throw new ReservedNameError();
   }
 

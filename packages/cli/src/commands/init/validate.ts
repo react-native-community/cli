c76ab9005cc3b0b1b37a76fe85ffa836a4663c9d
Mike Grabowski <grabbou@gmail.com>
2020-01-30 14:43:53 +0100

    Update validate.ts

--- packages/cli/src/commands/init/validate.ts
+++ packages/cli/src/commands/init/validate.ts
@@ -3,8 +3,61 @@ import ReservedNameError from './errors/ReservedNameError';
 import HelloWorldError from './errors/HelloWorldError';
 
 const NAME_REGEX = /^[$A-Z_][0-9A-Z_$]*$/i;
+
 // ref: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html
-const javaKeywords = ["abstract", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "synchronized", "boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw", "byte", "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally", "long", "strictfp", "volatile", "const", "float", "native", "super", "while"];
+const javaKeywords = [
+  'abstract',
+  'continue',
+  'for',
+  'new',
+  'switch',
+  'assert',
+  'default',
+  'goto',
+  'package',
+  'synchronized',
+  'boolean',
+  'do',
+  'if',
+  'private',
+  'this',
+  'break',
+  'double',
+  'implements',
+  'protected',
+  'throw',
+  'byte',
+  'else',
+  'import',
+  'public',
+  'throws',
+  'case',
+  'enum',
+  'instanceof',
+  'return',
+  'transient',
+  'catch',
+  'extends',
+  'int',
+  'short',
+  'try',
+  'char',
+  'final',
+  'interface',
+  'static',
+  'void',
+  'class',
+  'finally',
+  'long',
+  'strictfp',
+  'volatile',
+  'const',
+  'float',
+  'native',
+  'super',
+  'while',
+];
+
 const reservedNames = ['react', 'react-native', ...javaKeywords];
 
 export function validateProjectName(name: string) {

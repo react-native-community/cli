190708da43f932470e0e46a9f985af6b64c6ddf7
grabbou <grabbou@gmail.com>
2020-01-30 11:50:48 +0100

    fix typescript

--- packages/platform-android/src/commands/runAndroid/getLaunchPackageName.ts
+++ packages/platform-android/src/commands/runAndroid/getLaunchPackageName.ts
@@ -1,43 +1,42 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @flow
- */
-
 import fs from 'fs';
 import path from 'path';
 
-function findPreviousTerm(content, endPos) {
+function findPreviousTerm(content: string, endPos: number) {
   let pos = endPos;
 
   while (content[pos] === ' ') {
     --pos;
   }
+
   const regex = /\w/;
   const word = [];
+
   while (regex.exec(content[pos])) {
     word.push(content[pos]);
     --pos;
   }
+
   return word.reverse().join('');
 }
 
-function findBuildTypes(filePath) {
-  // Read the gradle file and get list of buildTypes defined for the project.
+/**
+ * Read the gradle file and get list of buildTypes defined for the project.
+ */
+function findBuildTypes(filePath: string) {
   const content = fs.readFileSync(filePath, 'utf8');
   const regex = /buildTypes+/gi;
   const buildTypes = [];
   const match = regex.exec(content);
+
   if (!match) {
     // Assume default types if buildTypes is not present
     return ['debug', 'release'];
   }
+
   const buildTypeStartPos = regex.lastIndex;
   let counter = 1;
   let pos = buildTypeStartPos + 1;
+
   while (counter > 0) {
     if (content[pos] === '{') {
       counter += 1;
@@ -52,37 +51,51 @@ function findBuildTypes(filePath) {
     }
     ++pos;
   }
+
   return buildTypes;
 }
 
-function splitVariant(gradleFilePath, variant) {
-  // Split the variant into buildType and flavor
+/**
+ *
+ * Split the variant into buildType and flavor
+ */
+function splitVariant(gradleFilePath: string, variant: string) {
   const buildTypes = findBuildTypes(gradleFilePath);
   const regexp = new RegExp(buildTypes.join('|'), 'gi');
   const match = regexp.exec(variant);
+
   let flavor = null;
   let buildType = variant;
+
   if (match) {
     flavor = variant.substring(0, match.index);
     buildType = variant.substring(match.index);
   }
+
   return {buildType, flavor};
 }
 
-function isSeparateBuildEnabled(gradleFilePath) {
-  // Check if separate is build enabled for different processors
+/**
+ * Check if separate is build enabled for different processors
+ */
+function isSeparateBuildEnabled(gradleFilePath: string) {
   const content = fs.readFileSync(gradleFilePath, 'utf8');
   const match = content.match(/(\w+)\senableSeparateBuildPerCPUArchitecture/);
+
   let separateBuild = '';
+
   if (match != null) {
     separateBuild = match[1];
   }
+
   return separateBuild.toLowerCase() === 'enable';
 }
 
-function getManifestFile(variant) {
-  // get the path to the correct manifest file to find the correct package name to be used while
-  // starting the app
+/**
+ * Get the path to the correct manifest file to find the correct package name to be used while
+ * starting the app
+ */
+function getManifestFile(variant: string) {
   const gradleFilePath = path.join('app', 'build.gradle');
 
   // We first need to identify build type and flavor from the specified variant
@@ -100,15 +113,14 @@ function getManifestFile(variant) {
 
   paths.push(buildType);
   paths.push('AndroidManifest.xml');
+
   return path.join(...paths);
 }
 
 export default function getLaunchPackageName(variant: string) {
-  // Get the complete launch path, as specified by the gradle build script
   const manifestFile = getManifestFile(variant || 'debug');
   const content = fs.readFileSync(manifestFile, 'utf8');
 
-  // Get the package name to launch, specified by the generated manifest file
   const matched = content.match(/package="(.+?)"/);
   let packageName = '';
 

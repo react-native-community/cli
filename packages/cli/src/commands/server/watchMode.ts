5007d6c186f70240ee30fea1037eca3e32958c2c
Mike Grabowski <grabbou@gmail.com>
2020-01-31 15:57:15 +0100

    Update watchMode.ts

--- packages/cli/src/commands/server/watchMode.ts
+++ packages/cli/src/commands/server/watchMode.ts
@@ -1,44 +1,42 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
 import readline from 'readline';
 import hookStdout from '../../tools/hookStdout';
 import {logger} from '@react-native-community/cli-tools';
 
 function printWatchModeInstructions() {
   logger.log(
-    `\n\nTo reload the app press "r"\nTo open developer menu press "d"`,
+    '\n\nTo reload the app press "r"\nTo open developer menu press "d"',
   );
 }
 
 function enableWatchMode(messageSocket: any) {
-  const {stdin} = process;
-  readline.emitKeypressEvents(stdin);
+  readline.emitKeypressEvents(process.stdin);
 
   // We need to set this to true to catch key presses individually.
   // As a result we have to implement our own method for exiting
   // and other commands (e.g. ctrl+c & ctrl+z)
-  stdin.setRawMode(true);
+  if (!process.stdin.setRawMode) {
+    logger.log('Watch mode is not supported in this environment');
+    return;
+  }
+
+  process.stdin.setRawMode(true);
 
   // We have no way of knowing when the dependency graph is done loading
   // except by hooking into stdout itself. We want to print instructions
   // right after its done loading.
   hookStdout(
-    output =>
+    (output: string) =>
       output.includes('Loading dependency graph, done.') &&
       printWatchModeInstructions(),
   );
 
-  stdin.on('keypress', (key, data) => {
+  process.stdin.on('keypress', (_key, data) => {
     const {ctrl, name} = data;
     if (ctrl === true) {
       switch (name) {
         case 'c':
           process.exit();
+          break;
         case 'z':
           process.emit('SIGTSTP');
           break;

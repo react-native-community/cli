b47f9bd9a46c0a977f10bed571c09ff2c4b0a183
ph0ph0 <phillipalanlee@googlemail.com>
2020-02-21 12:02:17 +0000

    changed implementation of needsToBeFixed and runAutomaticFix to remove react-native and react-native-cli using execa

--- packages/cli/src/commands/doctor/healthchecks/reactNativeGlobal.ts
+++ packages/cli/src/commands/doctor/healthchecks/reactNativeGlobal.ts
@@ -1,7 +1,10 @@
 import {HealthCheckInterface} from '../types';
 import fs from 'fs';
+import execa from 'execa';
 
 import resolveGlobal from 'resolve-global';
+import globalDirectories from 'global-dirs';
+import isPathInside from 'is-path-inside';
 
 const label = 'react-native-cli';
 
@@ -11,8 +14,41 @@ const checkGlobalInstall = (): boolean => {
   return !!((reactNativeCLIGlobal || reactNativeGlobal) && true);
 };
 
+const removeNodePackage = (packageName, packageManager) => {
+  packageManager === 'yarn'
+    ? execa(`yarn global remove ${packageName}`)
+    : execa(`npm uninstall -g ${packageName}`);
+};
+
 const automaticFix = () => {
-  
+  const reactNativePath = globalDirectories('react-native-cli');
+  const reactNativeCLIPath = globalDirectories('react-native');
+
+  if (isPathInside(reactNativePath, globalDirectories.yarn.packages)) {
+    removeNodePackage(reactNativePath, 'yarn');
+  }
+
+  if (
+    isPathInside(
+      reactNativePath,
+      fs.realpathSync(globalDirectories.npm.packages),
+    )
+  ) {
+    removeNodePackage(reactNativePath, 'npm');
+  }
+
+  if (isPathInside(reactNativeCLIPath, globalDirectories.yarn.packages)) {
+    removeNodePackage(reactNativeCLIPath, 'yarn');
+  }
+
+  if (
+    isPathInside(
+      reactNativeCLIPath,
+      fs.realpathSync(globalDirectories.npm.packages),
+    )
+  ) {
+    removeNodePackage(reactNativeCLIPath, 'npm');
+  }
 };
 
 export default {

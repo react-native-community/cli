672670dd927979b4a35893e8d0e7bffb0e1b7ec2
ph0ph0 <phillipalanlee@googlemail.com>
2020-02-21 08:59:07 +0000

    filled in automaticFix function

--- packages/cli/src/commands/doctor/healthchecks/reactNativeGlobal.ts
+++ packages/cli/src/commands/doctor/healthchecks/reactNativeGlobal.ts
@@ -1,18 +1,18 @@
 import {logger} from '@react-native-community/cli-tools';
 import {HealthCheckInterface} from '../types';
-
-const isInstalledGlobally = require('is-installed-globally');
+import fs from 'fs';
+import isInstalledGlobally from 'is-installed-globally';
+import resolveGlobal from 'resolve-global';
 
 const label = 'react-native-cli';
 
 const checkGlobalInstall = (): boolean => {
-  logger.log('Checking react-native-cli global install');
   return isInstalledGlobally('react-native-cli');
 };
 
-const automaticFix = () => {
-  // Get the path of the directory of globally installed react-native-cli
-  // Delete the directory of react-native-cli
+const automaticFix = async () => {
+  const reactNativeGlobalPath = resolveGlobal('react-native-cli');
+  fs.unlink(reactNativeGlobalPath, () => {});
 };
 
 export default {

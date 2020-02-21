75d80374e9ffe1369f238dc1a9e22e169be32092
Michał Pierzchała <thymikee@gmail.com>
2020-02-21 18:38:45 +0100

    chore: use script to link packages because lerna is buggy

---
+++ scripts/linkPackages.js
@@ -0,0 +1,15 @@
+const execa = require('execa');
+const chalk = require('chalk');
+const path = require('path');
+const glob = require('glob');
+
+const projects = glob
+  .sync('packages/*/package.json')
+  // We don't want to deal with global-cli at the moment
+  .filter(name => !name.includes('global-cli'));
+
+projects.forEach(project => {
+  const cwd = path.dirname(project);
+  console.log(chalk.dim(`Running "yarn link" in ${cwd}`));
+  execa.sync('yarn', ['link'], {cwd, stdio: 'inherit'});
+});

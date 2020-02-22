6fd67ec383367fa54187cca7fb1a8c1952c7862c
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-02-22 19:11:37 +0530

    chore: test case for default behavior

---
+++ __e2e__/default.test.ts
@@ -0,0 +1,16 @@
+import {run, getTempDirectory, cleanup, writeFiles} from '../jest/helpers';
+
+const DIR = getTempDirectory('test_default_behavior');
+
+beforeEach(() => {
+  cleanup(DIR);
+  writeFiles(DIR, {});
+});
+afterEach(() => {
+  cleanup(DIR);
+});
+
+test('shows up help information without passing in any args', () => {
+  const {stdout} = run(DIR);
+  expect(stdout).toMatchSnapshot();
+});

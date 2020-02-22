68d6c662f9cf894bdac538b82e0cbea45e48944a
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-02-22 16:48:09 +0530

    chore: test case for command suggestion feature

---
+++ __e2e__/validations.test.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+import {run, getTempDirectory, cleanup, writeFiles} from '../jest/helpers';
+
+const DIR = getTempDirectory('command-validations');
+
+beforeEach(() => {
+  cleanup(DIR);
+  writeFiles(DIR, {});
+});
+afterEach(() => {
+  cleanup(DIR);
+});
+
+test('suggests matching command', () => {
+  const {stderr} = run(
+    DIR,
+    ['ini'],
+    {expectedFailure: true},
+  );
+  expect(stderr).toContain('error Unrecognized command \"ini\". Did you mean \"init\"?');
+});

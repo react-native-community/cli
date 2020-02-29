962931106591e0828f001e2be7199298a60a042a
Antón Molleda <molant@users.noreply.github.com>
2020-02-29 13:22:10 -0800

    chore: refactor envinfo (#1025)

--- packages/cli/src/tools/envinfo.ts
+++ packages/cli/src/tools/envinfo.ts
@@ -2,20 +2,37 @@
 import envinfo from 'envinfo';
 import {EnvironmentInfo} from '../commands/doctor/types';
 
-export default async function getEnvironmentInfo() {
-  return JSON.parse(
-    await envinfo.run(
-      {
-        Binaries: ['Node', 'Yarn', 'npm', 'Watchman'],
-        IDEs: ['Xcode', 'Android Studio'],
-        SDKs: ['iOS SDK', 'Android SDK'],
-        npmPackages: ['react', 'react-native', '@react-native-community/cli'],
-        npmGlobalPackages: ['*react-native*'],
-      },
-      {
-        json: true,
-        showNotFound: true,
-      },
-    ),
-  ) as EnvironmentInfo;
+/**
+ * Returns information about the running system.
+ * If `json === true`, or no options are passed,
+ * the return type will be an `EnvironmentInfo`.
+ * If set to `false`, it will be a `string`.
+ */
+async function getEnvironmentInfo(): Promise<EnvironmentInfo>;
+async function getEnvironmentInfo(json: true): Promise<EnvironmentInfo>;
+async function getEnvironmentInfo(json: false): Promise<string>;
+async function getEnvironmentInfo(
+  json = true,
+): Promise<string | EnvironmentInfo> {
+  const options = {json, showNotFound: true};
+
+  const info = (await envinfo.run(
+    {
+      System: ['OS', 'CPU', 'Memory', 'Shell'],
+      Binaries: ['Node', 'Yarn', 'npm', 'Watchman'],
+      IDEs: ['Xcode', 'Android Studio'],
+      SDKs: ['iOS SDK', 'Android SDK'],
+      npmPackages: ['react', 'react-native', '@react-native-community/cli'],
+      npmGlobalPackages: ['*react-native*'],
+    },
+    options,
+  )) as string;
+
+  if (options.json) {
+    return JSON.parse(info);
+  }
+
+  return info.trim();
 }
+
+export default getEnvironmentInfo;

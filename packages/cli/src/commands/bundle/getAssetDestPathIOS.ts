47b3dd972024e1041b94d895f72912b8b3e1776d
Janic Duplessis <janicduplessis@gmail.com>
2020-03-11 14:13:06 -0600

    Fix bundling assets with a path outside of the project root (#939)

--- packages/cli/src/commands/bundle/getAssetDestPathIOS.ts
+++ packages/cli/src/commands/bundle/getAssetDestPathIOS.ts
@@ -12,7 +12,13 @@ import {PackagerAsset} from './assetPathUtils';
 function getAssetDestPathIOS(asset: PackagerAsset, scale: number): string {
   const suffix = scale === 1 ? '' : `@${scale}x`;
   const fileName = `${asset.name + suffix}.${asset.type}`;
-  return path.join(asset.httpServerLocation.substr(1), fileName);
+  return path.join(
+    // Assets can have relative paths outside of the project root.
+    // Replace `../` with `_` to make sure they don't end up outside of
+    // the expected assets directory.
+    asset.httpServerLocation.substr(1).replace(/\.\.\//g, '_'),
+    fileName,
+  );
 }
 
 export default getAssetDestPathIOS;

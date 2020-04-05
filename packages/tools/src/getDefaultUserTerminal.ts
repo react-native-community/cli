e87b6b566f75ee034393e5aaed02121df1a7aa79
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2020-04-05 22:26:17 +0200

    fix: Use correct terminal on linux systems (#1099)

--- packages/tools/src/getDefaultUserTerminal.ts
+++ packages/tools/src/getDefaultUserTerminal.ts
@@ -1,4 +1,17 @@
-const getDefaultUserTerminal = (): string | undefined =>
-  process.env.REACT_TERMINAL || process.env.TERM_PROGRAM;
+import os from 'os';
+
+const getDefaultUserTerminal = (): string | undefined => {
+  const {REACT_TERMINAL, TERM_PROGRAM, TERM} = process.env;
+
+  if (REACT_TERMINAL) {
+    return REACT_TERMINAL;
+  }
+
+  if (os.platform() === 'darwin') {
+    return TERM_PROGRAM;
+  }
+
+  return TERM;
+};
 
 export default getDefaultUserTerminal;

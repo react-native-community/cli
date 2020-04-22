080ffc6f187a526e82b21e52334e7b592552cbf9
simek <gosimek@gmail.com>
2020-04-22 15:49:08 +0200

    update docs link in the Android installation error message

--- packages/platform-android/src/commands/runAndroid/runOnAllDevices.ts
+++ packages/platform-android/src/commands/runAndroid/runOnAllDevices.ts
@@ -84,8 +84,7 @@ async function runOnAllDevices(
 
 function createInstallError(error: Error & {stderr: string}) {
   const stderr = (error.stderr || '').toString();
-  const docs =
-    'https://reactnative.dev/docs/getting-started.html#android-development-environment';
+  const docs = 'https://reactnative.dev/docs/environment-setup';
   let message = `Make sure you have the Android development environment set up: ${chalk.underline.dim(
     docs,
   )}`;

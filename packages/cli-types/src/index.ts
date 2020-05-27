0a93be1a42ed1fb05bb0ebf3b82d58b2dd920614
Eloy Durán <eloy.de.enige@gmail.com>
2020-05-27 12:04:45 +0200

    feat(cli-platform-ios): add macOS auto-linking support (#1126)
    * [platform-ios] Describe native_modules.rb input
    
    * [package] Add native_modules.rb to jest suite
    
    * [platform-ios/native_modules] Use CP API in tests
    
    * [platform-ios/native_modules] Port tests to JS
    
    * [platform-ios/native_modules] Add platform specificity
    
    Only include pods in targets that target platforms the pod supports.
    
    * [platform-ios/native_modules] Disable tests in unssuported envs
    
    * [platform-ios/native_modules] Clean-up test
    
    * [platform-ios/native_modules] Improve output

--- packages/cli-types/src/index.ts
+++ packages/cli-types/src/index.ts
@@ -3,6 +3,7 @@ import {
   IOSProjectParams,
   IOSDependencyConfig,
   IOSDependencyParams,
+  IOSNativeModulesConfig,
 } from './ios';
 import {
   AndroidProjectConfig,
@@ -128,7 +129,7 @@ export type ProjectConfig = {
  * @property platforms - Map of available platforms (build-ins and dynamically loaded)
  * @property commands - An array of commands that are present in 3rd party packages
  */
-export type Config = {
+export interface Config extends IOSNativeModulesConfig {
   root: string;
   reactNativePath: string;
   project: ProjectConfig;
@@ -150,7 +151,7 @@ export type Config = {
     [name: string]: PlatformConfig<any, any, any, any>;
   };
   commands: Command[];
-};
+}
 
 /**
  * Shares some structure with Config, except that root is calculated and can't
@@ -181,6 +182,7 @@ export {
   IOSProjectParams,
   IOSDependencyConfig,
   IOSDependencyParams,
+  IOSNativeModulesConfig,
 };
 
 export {

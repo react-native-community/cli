d46e3ea0b6958ac8823f0a77be5dfa014f288ab5
Andrew Coates <acoates-ms@noreply.github.com>
2020-08-25 08:15:51 -0700

    Just use the users default browser

--- packages/tools/src/launchDebugger.ts
+++ packages/tools/src/launchDebugger.ts
@@ -7,74 +7,10 @@
  * @format
  */
 
-import open from 'open';
-import {execSync} from 'child_process';
-import logger from './logger';
 import launchDefaultBrowser from './launchDefaultBrowser';
-import chalk from 'chalk';
-
-function commandExistsUnixSync(commandName: string) {
-  try {
-    const stdout = execSync(
-      `command -v ${commandName} 2>/dev/null` +
-        ` && { echo >&1 '${commandName} found'; exit 0; }`,
-    );
-    return !!stdout;
-  } catch (error) {
-    return false;
-  }
-}
-
-function getChromeAppName(): string {
-  switch (process.platform) {
-    case 'darwin':
-      return 'google chrome';
-    case 'win32': {
-      const possibleDebuggingApps = ['chrome.exe', 'msedge.exe'];
-      const output = execSync(
-        'tasklist /NH /FO CSV /FI "SESSIONNAME ne Services"',
-      ).toString();
-      const runningProcesses = output
-        .split('\n')
-        .map((line: string) => line.replace(/^"|".*\r$/gm, ''));
-
-      for (const app of possibleDebuggingApps) {
-        if (runningProcesses.includes(app)) {
-          return app;
-        }
-      }
-      return 'chrome';
-    }
-    case 'linux':
-      if (commandExistsUnixSync('google-chrome')) {
-        return 'google-chrome';
-      }
-      if (commandExistsUnixSync('chromium-browser')) {
-        return 'chromium-browser';
-      }
-      return 'chromium';
-
-    default:
-      return 'google-chrome';
-  }
-}
-
-function launchChrome(url: string) {
-  return open(url, {app: [getChromeAppName()], wait: true});
-}
 
 async function launchDebugger(url: string) {
-  try {
-    await launchChrome(url);
-  } catch (error) {
-    logger.debug(error);
-    logger.info(
-      `For a better debugging experience please install Google Chrome from: ${chalk.underline.dim(
-        'https://www.google.com/chrome/',
-      )}`,
-    );
-    launchDefaultBrowser(url);
-  }
+  return launchDefaultBrowser(url);
 }
 
 export default launchDebugger;

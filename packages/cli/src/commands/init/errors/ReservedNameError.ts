5773d65afd22f8e4fbad8219d3f75401a7b7dac1
ofirgeller <ofirgeller22@gmail.com>
2020-09-19 17:37:58 +0300

    default keyword if we consider the error public api

--- packages/cli/src/commands/init/errors/ReservedNameError.ts
+++ packages/cli/src/commands/init/errors/ReservedNameError.ts
@@ -1,5 +1,5 @@
 export default class ReservedNameError extends Error {
-  constructor(name: string) {
+  constructor(name: string = 'React') {
     super(
       `Not a valid name for a project. Please do not use the reserved word "${name}".`,
     );

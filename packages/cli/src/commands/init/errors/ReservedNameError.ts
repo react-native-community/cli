6bcfc9dc78fdd0889552ee86f3f0ff28374f9dcb
Mike Grabowski <grabbou@gmail.com>
2020-11-03 15:43:49 +0100

    Update ReservedNameError.ts

--- packages/cli/src/commands/init/errors/ReservedNameError.ts
+++ packages/cli/src/commands/init/errors/ReservedNameError.ts
@@ -1,5 +1,5 @@
 export default class ReservedNameError extends Error {
-  constructor(name: string = 'React') {
+  constructor(name: string) {
     super(
       `Not a valid name for a project. Please do not use the reserved word "${name}".`,
     );

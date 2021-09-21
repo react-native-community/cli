9f88c589aa22b4436e6dfce4f3b55ed1d3582223
Adam Foxman <adam@thefoxmans.net>
2021-09-21 14:46:34 -0700

    Make exports explicit

--- packages/cli-plugin-metro/src/index.ts
+++ packages/cli-plugin-metro/src/index.ts
@@ -1,3 +1,10 @@
-export * from './bundle';
+export {
+  bundle,
+  ramBundleCommand,
+  ramBundle,
+  AssetData,
+  buildBundle,
+  CommandLineArgs,
+} from './bundle';
 export * from './config';
 export * from './start';

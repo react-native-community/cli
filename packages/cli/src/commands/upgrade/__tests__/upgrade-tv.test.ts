4db22f0d0f31438e0d42ce3752064d8d4abb8857
Douglas Lowder <dlowder@salesforce.com>
2021-10-02 13:29:19 -0700

    Wrap upgrade tests in describes

--- packages/cli/src/commands/upgrade/__tests__/upgrade-tv.test.ts
+++ packages/cli/src/commands/upgrade/__tests__/upgrade-tv.test.ts
@@ -33,58 +33,62 @@ const newVersion = '0.64.2-4';
 const olderVersion = '0.60.2-1';
 const lessOlderVersion = '0.63.3-0';
 
-beforeEach(() => {
-  UpgradeTestingMethods.setup();
+describe('Upgrade tests for react-native-tvos repo', () => {
+  beforeEach(() => {
+    UpgradeTestingMethods.setup();
+  });
+
+  afterEach(() => {
+    UpgradeTestingMethods.teardown();
+  });
+
+  test('uses latest version of react-native when none passed', async () => {
+    await UpgradeTestingMethods.usesLatestVersionWhenNonePassed(repoName);
+  }, 60000);
+
+  test('applies patch in current working directory when nested', async () => {
+    await UpgradeTestingMethods.appliesPatchInCwdWhenNested(newVersion);
+  });
+
+  test('errors when invalid version passed', async () => {
+    await UpgradeTestingMethods.errorsWhenInvalidVersionPassed();
+  }, 60000);
+
+  test('errors when older version passed', async () => {
+    await UpgradeTestingMethods.errorsWhenOlderVersionPassed(
+      olderVersion,
+      lessOlderVersion,
+      currentVersion,
+    );
+  }, 60000);
+
+  test('warns when dependency upgrade version is in semver range', async () => {
+    await UpgradeTestingMethods.warnsWhenDependencyInSemverRange(
+      currentVersion,
+    );
+  }, 60000);
+
+  test('fetches empty patch and installs deps', async () => {
+    await UpgradeTestingMethods.fetchesEmptyPatchAndInstallsDeps(newVersion);
+  }, 60000);
+
+  test('fetches regular patch, adds remote, applies patch, installs deps, removes remote,', async () => {
+    await UpgradeTestingMethods.fetchesRegularPatchInstallRemoteAppliesPatchInstallsDepsRemovesRemote(
+      newVersion,
+    );
+  }, 60000);
+
+  test('fetches regular patch, adds remote, applies patch, installs deps, removes remote when updated from nested directory', async () => {
+    await UpgradeTestingMethods.fetchesRegularPatchInstallRemoteAppliesPatchInstallsDepsRemovesRemoteNested(
+      newVersion,
+    );
+  }, 60000);
+
+  test('cleans up if patching fails,', async () => {
+    await UpgradeTestingMethods.cleansUpIfPatchingFails(newVersion);
+  }, 60000);
+
+  test('works with --name-ios and --name-android', async () => {
+    await UpgradeTestingMethods.worksWithNameIosAndNameAndroid(newVersion);
+  }, 60000);
 });
-
-afterEach(() => {
-  UpgradeTestingMethods.teardown();
-});
-
-test('uses latest version of react-native when none passed', async () => {
-  await UpgradeTestingMethods.usesLatestVersionWhenNonePassed(repoName);
-}, 60000);
-
-test('applies patch in current working directory when nested', async () => {
-  await UpgradeTestingMethods.appliesPatchInCwdWhenNested(newVersion);
-});
-
-test('errors when invalid version passed', async () => {
-  await UpgradeTestingMethods.errorsWhenInvalidVersionPassed();
-}, 60000);
-
-test('errors when older version passed', async () => {
-  await UpgradeTestingMethods.errorsWhenOlderVersionPassed(
-    olderVersion,
-    lessOlderVersion,
-    currentVersion,
-  );
-}, 60000);
-
-test('warns when dependency upgrade version is in semver range', async () => {
-  await UpgradeTestingMethods.warnsWhenDependencyInSemverRange(currentVersion);
-}, 60000);
-
-test('fetches empty patch and installs deps', async () => {
-  await UpgradeTestingMethods.fetchesEmptyPatchAndInstallsDeps(newVersion);
-}, 60000);
-
-test('fetches regular patch, adds remote, applies patch, installs deps, removes remote,', async () => {
-  await UpgradeTestingMethods.fetchesRegularPatchInstallRemoteAppliesPatchInstallsDepsRemovesRemote(
-    newVersion,
-  );
-}, 60000);
-
-test('fetches regular patch, adds remote, applies patch, installs deps, removes remote when updated from nested directory', async () => {
-  await UpgradeTestingMethods.fetchesRegularPatchInstallRemoteAppliesPatchInstallsDepsRemovesRemoteNested(
-    newVersion,
-  );
-}, 60000);
-
-test('cleans up if patching fails,', async () => {
-  await UpgradeTestingMethods.cleansUpIfPatchingFails(newVersion);
-}, 60000);
-
-test('works with --name-ios and --name-android', async () => {
-  await UpgradeTestingMethods.worksWithNameIosAndNameAndroid(newVersion);
-}, 60000);

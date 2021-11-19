8bb84b0567aa7ce2cbcdf829d6a52d27f13a34b4
Dulmandakh <dulmandakh@gmail.com>
2021-11-19 09:25:50 +0800

    fix unittest

--- packages/cli/src/commands/link/__tests__/makeHook.test.ts
+++ packages/cli/src/commands/link/__tests__/makeHook.test.ts
@@ -8,12 +8,12 @@ describe('makeHook', () => {
   it('invokes the command', async () => {
     const hook = makeHook('echo');
     const result = await hook();
-    expect(result.cmd).toBe('echo');
+    expect(result.command).toBe('echo');
   });
 
   it('invokes the command with multiple arguments', async () => {
     const hook = makeHook('node -p "1;"');
     const result = await hook();
-    expect(result.cmd).toBe('node -p "1;"');
+    expect(result.command).toBe('node -p "1;"');
   });
 });

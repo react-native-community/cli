1be13ae1f187c0446cf5bf87d2bba71452f492d4
Michel Weststrate <mweststrate@fb.com>
2021-12-22 10:58:00 +0000

    Fix lint error

--- packages/cli-server-api/src/websocket/eventsSocketServer.ts
+++ packages/cli-server-api/src/websocket/eventsSocketServer.ts
@@ -128,7 +128,11 @@ function attachToServer(
       // origin is only checked if it is set, e.g. when the request is made from a (CORS) browser
       // any 'back-end' connection isn't CORS at all, and has full control over the origin header,
       // so there is no point in checking it security wise
-      return !origin || origin.startsWith('http://localhost:') || origin.startsWith('file:');
+      return (
+        !origin ||
+        origin.startsWith('http://localhost:') ||
+        origin.startsWith('file:')
+      );
     },
   });
 

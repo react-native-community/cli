8086cc4bfdd999182ebce22a260164026b8ca1f9
Mike Grabowski <grabbou@gmail.com>
2022-01-25 15:55:46 +0100

    fix: add note about todo

--- packages/cli-doctor/src/index.ts
+++ packages/cli-doctor/src/index.ts
@@ -3,4 +3,9 @@ import info from './commands/info';
 
 export const commands = {info, doctor};
 
+/**
+ * @todo
+ * We should not rely on this file from other packages, e.g. CLI. We probably need to 
+ * refactor the init in order to remove that connection.
+ */
 export {default as installPods} from './tools/installPods';

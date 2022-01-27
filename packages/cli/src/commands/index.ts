e1515636b2fd1c64f03c0a0fad6bb505f9ce973e
Mike Grabowski <grabbou@gmail.com>
2022-01-27 12:18:57 +0100

    chore: initial commit - remove files

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -3,10 +3,6 @@ import {commands as doctorCommands} from '@react-native-community/cli-doctor';
 import {commands as configCommands} from '@react-native-community/cli-config';
 import {commands as metroCommands} from '@react-native-community/cli-plugin-metro';
 import profileHermes from '@react-native-community/cli-hermes';
-import link from './link/link';
-import unlink from './link/unlink';
-import install from './install/install';
-import uninstall from './install/uninstall';
 import upgrade from './upgrade/upgrade';
 import init from './init';
 
@@ -14,10 +10,6 @@ export const projectCommands = [
   ...metroCommands,
   ...configCommands,
   doctorCommands.info,
-  link,
-  unlink,
-  install,
-  uninstall,
   upgrade,
   profileHermes,
 ] as Command[];

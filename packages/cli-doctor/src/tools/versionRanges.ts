226bd9fcd8fdb712d9b425bf5f75c39e80756057
Mike Hardy <github@mikehardy.net>
2022-02-11 13:32:18 -0500

    build(deps)!: JDK11 and SDK 31+ are required
    The template now inits a compileSdk/targetSdk 31 project,
    JDK8 cannot compile reliably against SDK31, so bump JDK as well

--- packages/cli-doctor/src/tools/versionRanges.ts
+++ packages/cli-doctor/src/tools/versionRanges.ts
@@ -4,9 +4,9 @@ export default {
   YARN: '>= 1.10.x',
   NPM: '>= 4.x',
   WATCHMAN: '>= 4.x',
-  JAVA: '1.8.x || >= 9',
+  JAVA: '>= 11',
   // Android
-  ANDROID_SDK: '>= 26.x',
+  ANDROID_SDK: '>= 31.x',
   ANDROID_NDK: '>= 19.x',
   // iOS
   XCODE: '>= 12.x',

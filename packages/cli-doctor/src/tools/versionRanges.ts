0f29b260c9bc90ec2aa484a3521d63f4dc17df25
Mike Hardy <github@mikehardy.net>
2022-02-14 16:45:54 -0500

    fix(doctor, watchman): accept new date-based watchman versions (#1550)
    * test(doctor, watchman): add a unit test for watchman functionality
    
    this verifies existing functionality prior to changes
    
    * fix(doctor, watchman): accept new date-based watchman versions
    
    doctor was giving false-negatives with the new watchman versions
    
    they use date-based versions now that violate semver with leading-zeros in numeric parts:
    - https://github.com/npm/node-semver/issues/232
    
    loose mode correctly pasrses them though, so add optional parameter to install check that
    allows loose version range coercion
    
    Should be compatible as watchman project is on record as being permanently backwards-compatible:
    https://facebook.github.io/watchman/docs/compatibility.html

--- packages/cli-doctor/src/tools/versionRanges.ts
+++ packages/cli-doctor/src/tools/versionRanges.ts
@@ -3,7 +3,7 @@ export default {
   NODE_JS: '>= 12',
   YARN: '>= 1.10.x',
   NPM: '>= 4.x',
-  WATCHMAN: '4.x',
+  WATCHMAN: '>= 4.x',
   JAVA: '1.8.x || >= 9',
   // Android
   ANDROID_SDK: '>= 26.x',

cb1d9df5c8964c49861e3740487d92d2caf0d272
victor gazolli <victorlpgazolli@gmail.com>
2022-05-25 09:32:25 -0300

    refactor: eslint fix

--- packages/tools/src/throwIfNonHttpProtocol.ts
+++ packages/tools/src/throwIfNonHttpProtocol.ts
@@ -1,21 +1,20 @@
 /**
  * Check if a string is an http/https url
  */
-export default function throwIfNonHttpProtocol(
-    url: string,
-) {
-    const _url = new URL(url);
+export default function throwIfNonHttpProtocol(url: string) {
+  const _url = new URL(url);
 
-    const urlProtocol = _url.protocol;
+  const urlProtocol = _url.protocol;
 
-    const expectedProtocol = {
-        [urlProtocol]: false,
-        "http:": true,
-        "https:": true,
-    }
+  const expectedProtocol = {
+    [urlProtocol]: false,
+    'http:': true,
+    'https:': true,
+  };
 
-    const isFromExpectedProtocol = expectedProtocol[urlProtocol];
+  const isFromExpectedProtocol = expectedProtocol[urlProtocol];
 
-    if (!isFromExpectedProtocol) throw new Error("invalid url, missing http/https protocol");
-
+  if (!isFromExpectedProtocol) {
+    throw new Error('invalid url, missing http/https protocol');
+  }
 }

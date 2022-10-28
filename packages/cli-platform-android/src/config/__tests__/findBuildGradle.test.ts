cfab726fee28e09287c9b37fd5c0b917d9de3a4a
Nicola Corti <corti.nico@gmail.com>
2022-10-28 14:51:31 +0100

    Fix findBuildGradle for app & libraries (#1728)
    * Fix findBuildGradle for app & libraries
    
    * Remove extra parameter

---
+++ packages/cli-platform-android/src/config/__tests__/findBuildGradle.test.ts
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import {findBuildGradle} from '../findBuildGradle';
+import * as mocks from '../__fixtures__/android';
+
+jest.mock('path');
+jest.mock('fs');
+
+const fs = require('fs');
+
+describe('findBuildGradle for apps', () => {
+  beforeAll(() => {
+    fs.__setMockFilesystem({
+      empty: {},
+      flat: {
+        android: mocks.validApp,
+      },
+    });
+  });
+
+  it('returns the app gradle path if file exists in the folder', () => {
+    expect(findBuildGradle('/flat/android', false)).toBe(
+      '/flat/android/app/build.gradle',
+    );
+  });
+
+  it('returns `null` if there is no gradle in the app folder', () => {
+    expect(findBuildGradle('/empty', false)).toBeNull();
+  });
+});
+
+describe('findBuildGradle for libraries', () => {
+  beforeAll(() => {
+    fs.__setMockFilesystem({
+      empty: {},
+      flat: {
+        android: mocks.valid,
+      },
+    });
+  });
+
+  it('returns the app gradle path if file exists in the folder', () => {
+    expect(findBuildGradle('/flat/android', true)).toBe(
+      '/flat/android/build.gradle',
+    );
+  });
+
+  it('returns `null` if there is no gradle in the app folder', () => {
+    expect(findBuildGradle('/empty', true)).toBeNull();
+  });
+});

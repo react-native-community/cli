3d47e464b6cf7655bf687bb0f28ce0053641bfa2
mdneyazahmad <77761491+mdneyazahmad@users.noreply.github.com>
2022-10-31 15:55:02 +0530

    fix: select iPhone 14 as a default simulator (#1730)

--- packages/cli-platform-ios/src/commands/runIOS/index.ts
+++ packages/cli-platform-ios/src/commands/runIOS/index.ts
@@ -594,7 +594,7 @@ export default {
       description:
         'Explicitly set simulator to use. Optionally include iOS version between ' +
         'parenthesis at the end to match an exact version: "iPhone 6 (10.0)"',
-      default: 'iPhone 13',
+      default: 'iPhone 14',
     },
     {
       name: '--configuration <string>',

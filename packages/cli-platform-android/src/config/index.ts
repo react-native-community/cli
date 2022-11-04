1dde377158d2fd76716599f9b8955c33f5856bf2
Michał Pierzchała <thymikee@gmail.com>
2022-11-04 10:48:04 +0100

    Update packages/cli-platform-android/src/config/index.ts

--- packages/cli-platform-android/src/config/index.ts
+++ packages/cli-platform-android/src/config/index.ts
@@ -135,7 +135,7 @@ export function dependencyConfig(
   let cmakeListsPath = userConfig.cmakeListsPath
     ? path.join(sourceDir, userConfig.cmakeListsPath)
     : path.join(sourceDir, 'build/generated/source/codegen/jni/CMakeLists.txt');
-  if(process.platform == "win32") {
+  if (process.platform === "win32") {
     cmakeListsPath = cmakeListsPath.replace(/\\/g, "/");
   }
   return {

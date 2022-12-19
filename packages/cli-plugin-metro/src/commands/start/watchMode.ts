e772c67ae9eec5d5d0d262e3a7a762910d305fe4
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2022-12-19 08:20:26 +0100

    feat: run app with hotkey

--- packages/cli-plugin-metro/src/commands/start/watchMode.ts
+++ packages/cli-plugin-metro/src/commands/start/watchMode.ts
@@ -1,9 +1,16 @@
 import readline from 'readline';
+import fs from 'fs';
 import {logger, hookStdout} from '@react-native-community/cli-tools';
+import execa from 'execa';
+import chalk from 'chalk';
 
 function printWatchModeInstructions() {
   logger.log(
-    '\n\nTo reload the app press "r"\nTo open developer menu press "d"',
+    `${chalk.bold('r')} - reload the app\n${chalk.bold(
+      'd',
+    )} - open developer menu\n${chalk.bold('i')} - run on iOS\n${chalk.bold(
+      'a',
+    )} - run on Android`,
   );
 }
 
@@ -47,6 +54,14 @@ function enableWatchMode(messageSocket: any) {
     } else if (name === 'd') {
       messageSocket.broadcast('devMenu', null);
       logger.info('Opening developer menu...');
+    } else if (name === 'i' || name === 'a') {
+      const isUsingYarn = fs
+        .readdirSync(`${process.cwd()}`)
+        .includes('yarn.lock');
+      logger.info(`Opening the app on ${name === 'i' ? 'iOS' : 'Android'}...`);
+      execa(isUsingYarn ? 'yarn' : 'npm run', [
+        name === 'i' ? 'ios' : 'android',
+      ]).stdout?.pipe(process.stdout);
     } else {
       console.log(_key);
     }

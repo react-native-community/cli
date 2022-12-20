a008119d5d82c220fe4a9727aeb460c2cf711f24
szymonrybczak <szymon.rybczak@gmail.com>
2022-12-20 10:54:08 +0100

    feat: add condition for min node version

--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
 
 import './tools/gracefulifyFs';
+import semver from 'semver';
+import execa from 'execa';
+import chalk from 'chalk';
 
-import {run} from './';
+const nodeVersion = execa.sync('node', ['--version'], {}).stdout;
 
-run();
+if (semver.satisfies(nodeVersion, '>=14')) {
+  const {run} = require('./');
+  run();
+} else {
+  console.error(`${chalk.red('You need at least Node 14 to run CLI.')}`);
+}

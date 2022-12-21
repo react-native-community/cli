7618a676a8901758dbd8eea9b096206859bdca65
Szymon Rybczak <szymon.rybczak@gmail.com>
2022-12-21 11:17:24 +0100

    feat: add condition for min node version (#1782)
    * feat: add condition for min node version
    
    * fix: change way to get node version
    
    * fix: use min node version from versionRanges
    
    * chore: change error message

--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
 
 import './tools/gracefulifyFs';
+import semver from 'semver';
+import chalk from 'chalk';
+import {versionRanges} from '@react-native-community/cli-doctor';
 
-import {run} from './';
-
-run();
+if (semver.satisfies(process.version, versionRanges.NODE_JS)) {
+  const {run} = require('./');
+  run();
+} else {
+  console.error(
+    `${chalk.red(
+      `React Native needs Node.js ${versionRanges.NODE_JS}. You're currently on version ${process.version}. Please upgrade Node.js to a supported version and try again.`,
+    )}`,
+  );
+}

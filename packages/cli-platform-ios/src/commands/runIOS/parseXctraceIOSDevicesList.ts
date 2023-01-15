d36fb10acfef849b1d1cf6e6a0a1e0d4e81a05cb
Simon Köck <54739715+simonkoeck@users.noreply.github.com>
2023-01-15 14:38:26 +0100

    Add devices check in parseXctraceIOSDevicesList

--- packages/cli-platform-ios/src/commands/runIOS/parseXctraceIOSDevicesList.ts
+++ packages/cli-platform-ios/src/commands/runIOS/parseXctraceIOSDevicesList.ts
@@ -27,6 +27,13 @@ function parseIOSDevicesList(text: string): Array<Device> {
   const devices: Array<Device> = [];
   let isSimulator = false;
 
+  if (
+    text.indexOf('== Simulators ==') === -1 &&
+    text.indexOf('== Devices ==') === -1
+  ) {
+    return [];
+  }
+
   text.split('\n').forEach((line) => {
     if (line === '== Simulators ==') {
       isSimulator = true;

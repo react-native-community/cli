a3a0777ba664a9e46f6f119c33437050a9ef2319
Sunbreak <sunbreak.wang@gmail.com>
2023-02-15 19:30:04 +0800

    fix parse matchArray result

--- packages/cli-platform-android/src/config/getAndroidProject.ts
+++ packages/cli-platform-android/src/config/getAndroidProject.ts
@@ -96,7 +96,7 @@ export function parseNamespaceFromBuildGradleFile(buildGradle: string) {
   // search for namespace = inside the build.gradle file via regex
   const matchArray = buildGradle.match(/namespace\s*[=]*\s*("(.+?)"|'(.+?)')/);
   if (matchArray && matchArray.length > 0) {
-    return matchArray[1];
+    return matchArray[2];
   } else {
     return null;
   }

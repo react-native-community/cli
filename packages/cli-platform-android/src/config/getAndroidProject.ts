cf8509fccde92294e0abbb3f0528903fb494e463
Sunbreak <sunbreak.wang@gmail.com>
2023-02-16 19:53:02 +0800

    use matchArray[1]

--- packages/cli-platform-android/src/config/getAndroidProject.ts
+++ packages/cli-platform-android/src/config/getAndroidProject.ts
@@ -94,9 +94,9 @@ export function parsePackageNameFromAndroidManifestFile(
 
 export function parseNamespaceFromBuildGradleFile(buildGradle: string) {
   // search for namespace = inside the build.gradle file via regex
-  const matchArray = buildGradle.match(/namespace\s*[=]*\s*("(.+?)"|'(.+?)')/);
+  const matchArray = buildGradle.match(/namespace\s*[=]*\s*["'](.+?)["']/);
   if (matchArray && matchArray.length > 0) {
-    return matchArray[2] ?? matchArray[3];
+    return matchArray[1];
   } else {
     return null;
   }

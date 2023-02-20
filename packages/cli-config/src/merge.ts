a510b91972cb1a9b10fe64e35ce46c7bf2f082a3
Michał Pierzchała <thymikee@gmail.com>
2023-02-20 18:20:45 +0100

    fixup deepmerge

--- packages/cli-config/src/merge.ts
+++ packages/cli-config/src/merge.ts
@@ -4,14 +4,9 @@ import deepmerge from 'deepmerge';
  * `deepmerge` concatenates arrays by default instead of overwriting them.
  * We define custom merging function for arrays to change that behaviour
  */
-export default function merge<X, Y>(x: X, y: Y) {
-  return deepmerge(
-    // @ts-ignore todo
-    x,
-    y,
-    {
-      arrayMerge: (_destinationArray: any[], sourceArray: any[]): any[] =>
-        sourceArray,
-    },
-  );
+export default function merge<X, Y>(x: Partial<X>, y: Partial<Y>) {
+  return deepmerge(x, y, {
+    arrayMerge: (_destinationArray: any[], sourceArray: any[]): any[] =>
+      sourceArray,
+  });
 }

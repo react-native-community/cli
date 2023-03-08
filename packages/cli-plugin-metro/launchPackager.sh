10dda2147c8d68f3b63a4211b15ebd45da454bd2
szymonrybczak <szymon.rybczak@gmail.com>
2023-03-08 11:52:43 +0100

    fix: add missing process terminated to `launchPackager.sh`

--- packages/cli-plugin-metro/launchPackager.sh
+++ packages/cli-plugin-metro/launchPackager.sh
@@ -2,3 +2,8 @@ THIS_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOUR
 source "$THIS_DIR/.packager.env"
 cd $PROJECT_ROOT
 $REACT_NATIVE_PATH/cli.js start
+
+if [[ -z "$CI" ]]; then
+  echo "Process terminated. Press <enter> to close the window"
+  read -r
+fi

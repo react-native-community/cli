0296d31b53122569687636f11ffbc324ade104b7
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-08-19 12:51:33 +0000

    fix(ios): fix crash when `--mode` is specified (#1958)
    Assumptions about where the `.xcodeproj` lives causes
    `checkIfConfigurationExists` to crash because `undefined` was passed as
    project.

--- packages/cli-platform-ios/src/tools/checkIfConfigurationExists.ts
+++ packages/cli-platform-ios/src/tools/checkIfConfigurationExists.ts
@@ -1,10 +1,15 @@
-import {CLIError} from '@react-native-community/cli-tools';
+import {CLIError, logger} from '@react-native-community/cli-tools';
 import {IosProjectInfo} from '../types';
 
 export function checkIfConfigurationExists(
   project: IosProjectInfo,
   mode: string,
 ) {
+  if (!project) {
+    logger.warn(`Unable to check whether "${mode}" exists in your project`);
+    return;
+  }
+
   if (!project.configurations.includes(mode)) {
     throw new CLIError(
       `Configuration "${mode}" does not exist in your project. Please use one of the existing configurations: ${project.configurations.join(

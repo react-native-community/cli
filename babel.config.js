0ca62cc1bb5bd183803033e5ec2a22555c0b0fa7
szymonrybczak <szymon.rybczak@gmail.com>
2023-11-01 13:39:58 +0100

    chore: bump node to 20

--- babel.config.js
+++ babel.config.js
@@ -10,7 +10,7 @@ module.exports = {
     [
       require.resolve('@babel/preset-env'),
       {
-        targets: {node: '18'},
+        targets: {node: '20'},
         useBuiltIns: 'entry',
         corejs: '2.x',
       },

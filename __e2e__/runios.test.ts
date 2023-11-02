9cce9f9ba23bb523b3a06b9b83fe79d396f74a94
szymonrybczak <szymon.rybczak@gmail.com>
2023-11-02 14:36:18 +0100

    feat(e2e): add tests for `run-ios`

---
+++ __e2e__/runios.test.ts
@@ -0,0 +1,51 @@
+import path from 'path';
+
+import {
+  spawnScript,
+  runCLI,
+  getTempDirectory,
+  cleanup,
+  writeFiles,
+  addRNCPrefix,
+  getAllPackages,
+} from '../jest/helpers';
+
+const cwd = getTempDirectory('test_different_roots');
+
+beforeAll(() => {
+  const packages = getAllPackages();
+
+  // Register all packages to be linked
+  for (const pkg of packages) {
+    spawnScript('yarn', ['link'], {
+      cwd: path.join(__dirname, `../packages/${pkg}`),
+    });
+  }
+
+  // Clean up folder and re-create a new project
+  cleanup(cwd);
+  writeFiles(cwd, {});
+
+  // Initialise React Native project
+  runCLI(cwd, ['init', 'TestProject', `--install-pods`]);
+
+  // Link CLI to the project
+
+  spawnScript('yarn', ['link', ...addRNCPrefix(packages)], {
+    cwd: path.join(cwd, 'TestProject'),
+  });
+});
+
+afterAll(() => {
+  cleanup(cwd);
+});
+
+test('`run-ios`', () => {
+  const result = runCLI(path.join(cwd, 'TestProject'), [
+    'run-ios',
+    '--no-packager',
+  ]);
+  expect(result).toContain(
+    'success Successfully launched the app on the simulator',
+  );
+});

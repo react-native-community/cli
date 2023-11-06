fac59f23b7b3fcf319cf0d01fd91294f160d8f93
szymonrybczak <szymon.rybczak@gmail.com>
2023-11-06 22:45:41 +0100

    fix(e2e): update yarn specific files

--- __e2e__/init.test.ts
+++ __e2e__/init.test.ts
@@ -157,9 +157,10 @@ test('init uses npm as the package manager with --npm', () => {
 
   const initDirPath = path.join(DIR, PROJECT_NAME);
 
-  // Remove yarn.lock and node_modules
+  // Remove yarn specific files and node_modules
   const filteredFiles = customTemplateCopiedFiles.filter(
-    (file) => !['yarn.lock', 'node_modules'].includes(file),
+    (file) =>
+      !['yarn.lock', 'node_modules', '.yarnrc.yml', '.yarn'].includes(file),
   );
 
   // Add package-lock.json

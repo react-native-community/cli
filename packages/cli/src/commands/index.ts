5855e0bc6aefc95459e2d993992c57d3a7915176
Blake Friedman <blakef@meta.com>
2024-03-04 11:51:21 +0000

    chore: RFC-0759 remove upgrade command (#2317)

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -3,14 +3,12 @@ import {commands as cleanCommands} from '@react-native-community/cli-clean';
 import {commands as doctorCommands} from '@react-native-community/cli-doctor';
 import {commands as configCommands} from '@react-native-community/cli-config';
 import profileHermes from '@react-native-community/cli-hermes';
-import upgrade from './upgrade/upgrade';
 import init from './init';
 
 export const projectCommands = [
   ...configCommands,
   cleanCommands.clean,
   doctorCommands.info,
-  upgrade,
   profileHermes,
 ] as Command[];
 

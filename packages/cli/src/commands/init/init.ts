5ddc8bb96cb5c9832e448261b66c2c70d51f4ccd
islandryu <shimaryuhei@gmail.com>
2024-05-18 07:39:51 +0900

    fix: Display correct error when config file does not exist

--- packages/cli/src/commands/init/init.ts
+++ packages/cli/src/commands/init/init.ts
@@ -337,8 +337,9 @@ async function createFromTemplate({
       loader.succeed('Dependencies installation skipped');
     }
   } catch (e) {
-    loader.fail();
-    if (e instanceof Error) {
+    if (e instanceof CLIError) {
+      logger.error(e.message);
+    } else if (e instanceof Error) {
       logger.error(
         'Installing pods failed. This doesn\'t affect project initialization and you can safely proceed. \nHowever, you will need to install pods manually when running iOS, follow additional steps in "Run instructions for iOS" section.\n',
       );

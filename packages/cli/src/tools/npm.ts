dd1e167262ff7ae8249e73d06042a22152649e73
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-07-15 13:21:05 +0200

    fix `npm config` for monorepos
    Co-authored-by: Szymon Rybczak <szymon.rybczak@gmail.com>

--- packages/cli/src/tools/npm.ts
+++ packages/cli/src/tools/npm.ts
@@ -36,7 +36,7 @@ export const getNpmRegistryUrl = (() => {
   return () => {
     if (!registryUrl) {
       try {
-        registryUrl = execSync('npm config get registry').toString().trim();
+        registryUrl = execSync('npm config get registry --workspaces=false --include-workspace-root').toString().trim();
       } catch {
         registryUrl = 'https://registry.npmjs.org/';
       }

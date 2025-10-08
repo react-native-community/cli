45d6b0984893df6b7b4a321a112a86f1918673df
Ben McCann <322311+benmccann@users.noreply.github.com>
2025-10-08 13:00:13 -0700

    fix unixifyPaths

--- packages/cli-tools/src/unixifyPaths.ts
+++ packages/cli-tools/src/unixifyPaths.ts
@@ -7,5 +7,5 @@
  */
 
 export default function unixifyPaths(path: string): string {
-  return path.replace(/^([a-zA-Z]+:|\.\/)/, '');
+  return path.replace(/^([a-zA-Z]+:|\.\/)/, '').replace(/\\/g, '/');
 }
